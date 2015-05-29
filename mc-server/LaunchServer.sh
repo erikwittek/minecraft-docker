3ee75ff65b85726bdf49b9cabd0e2122a966caff
Erik Wittek <erik@webhippie.de>
2015-05-29 21:31:21 +0200

    server fix

--- mc-server/LaunchServer.sh
+++ mc-server/LaunchServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx6G -XX:MaxPermSize=256M -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
+java -Xmx2G -XX:MaxPermSize=256M -jar forge-1.7.10-10.13.3.1401-1710ls-universal.jar nogui
